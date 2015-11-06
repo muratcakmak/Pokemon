4d8ec45828adfcfe7f2e3840abb90183dfee3bee
Murat Cakmak <oguzhanmc@gmail.com>
2015-11-06 08:26:41 -0500

    Logo, music button, mp3 file and AVAudioPlayer are added

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
 
@@ -14,13 +16,34 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
     
     var pokemon = [Pokemon]()
     
+    var musicPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionViewPokemon.delegate = self
         collectionViewPokemon.dataSource = self
         parserCSVtoArray()
+        
+        playMusic()
     }
+    
+    func playMusic(){
+        let path = NSBundle.mainBundle().pathForResource("music", ofType: "mp3")!
+        
+        do{
+            musicPlayer = try AVAudioPlayer(contentsOfURL: NSURL(string: path)!)
+            musicPlayer.prepareToPlay()
+            musicPlayer.numberOfLoops = -1
+            musicPlayer.play()
+            
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
+    
+    }
+    
     
     
     func parserCSVtoArray(){
@@ -71,5 +94,16 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         return CGSizeMake(105, 105)
     }
     
+    @IBAction func musicButton(sender: UIButton!) {
+        if musicPlayer.playing{
+            musicPlayer.stop()
+            sender.alpha = 0.2
+        }else{
+            musicPlayer.play()
+            sender.alpha = 1.0
+        }
+        
+        
+    }
 }
 
